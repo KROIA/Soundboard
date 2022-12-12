3f5d4265ea8e6eae8b40bf37b68e0590664cd9d7
Praxxley <bstagen@msn.com>
2022-12-12 15:30:18 +0100

    Syntaxanpassung

--- inc/ui/ui_sound.h
+++ inc/ui/ui_sound.h
@@ -28,130 +28,130 @@ class UI_Sound : public QWidget//, public XmlSaveable
 
         /**
          * getSource()
-         * @return Returns the source object to the .mp3 file
+         * \return Returns the source object to the .mp3 file
          */
         const SoundSource &getSource() const;
 
         /**
          * getVolume()
-         * @return Returns the volume of the sound
+         * \return Returns the volume of the sound
          */
         float getVolume() const;
 
         /**
          * getPlaybackSpeed()
-         * @return Returns the playback speed of the sound
+         * \return Returns the playback speed of the sound
          */
         float getPlaybackSpeed() const;
 
         /**
          * getLoops()
-         * @return Returns the amount of loops the sound will play
+         * \return Returns the amount of loops the sound will play
          */
         unsigned int getLoops() const;
 
         /**
          * getName()
-         * @return Returns the name of the sound
+         * \return Returns the name of the sound
          */
         const std::string &getName() const;
 
         /**
          * save(...)
-         * @brief Implementation of XmlSaveable
-         * @param writer QXmlStreamWriter which will save the file
+         * \brief Implementation of XmlSaveable
+         * \param writer QXmlStreamWriter which will save the file
          */
         //void save(QXmlStreamWriter *writer) override;
 
         /**
          * load(...)
-         * @brief Implementation of XmlSaveable
-         * @param reader QXmlStreamReader which will read from the file
+         * \brief Implementation of XmlSaveable
+         * \param reader QXmlStreamReader which will read from the file
          */
         //void load(QXmlStreamReader *reader) override;
 
     signals:
         /**
          * onPlaybackFinished()
-         * @brief Will be called if the playback is finished
+         * \brief Will be called if the playback is finished
          */
         void onPlaybackFinished();
 
         /**
          * onPlaybackPaused()
-         * @brief Will be called if the playback is paused
+         * \brief Will be called if the playback is paused
          */
         void onPlaybackPaused();
 
         /**
          * onPlaybackStopped()
-         * @brief Will be called if the playback is stopped
+         * \brief Will be called if the playback is stopped
          */
         void onPlaybackStopped();
 
     public slots:
         /**
          * play()
-         * @brief Will start the playback
+         * \brief Will start the playback
          */
         void play();
 
         /**
          * pause()
-         * @brief Will pause the current playback
+         * \brief Will pause the current playback
          */
         void pause();
 
         /**
          * stop()
-         * @brief Will stop the current playback
+         * \brief Will stop the current playback
          */
         void stop();
 
         /**
          * setSource(...)
-         * @param source The source object which contains the path to
+         * \param source The source object which contains the path to
          *               the .mp3 file
          */
         void setSource(const SoundSource &source);
 
         /**
          * setVolume(...)
-         * @param volume The volume of the playback.
+         * \param volume The volume of the playback.
          *               Range [0 - 1]
          */
         void setVolume(float volume);
 
         /**
          * setPlaybackSpeed(...)
-         * @param speed The speed of the playback.
+         * \param speed The speed of the playback.
          *              Default = 1, x<1 = faster
          */
         void setPlaybackSpeed(float speed);
 
         /**
          * setLoops(...)
-         * @param count The amount for how often the playback will be repeated.
+         * \param count The amount for how often the playback will be repeated.
          *              QMediaPlayer::Infinite for infinite playback
          */
         void setLoops(int count);
 
         /**
          * toggleRepeat()
-         * @brief Will toggle the autorepeat mode
+         * \brief Will toggle the autorepeat mode
          */
         void toggleRepeat(bool toggleOn);
 
         /**
          * isRepeating()
-         * @return true  If the sound is in loop mode
+         * \return true  If the sound is in loop mode
          *         false If the sound is not in loop mode
          */
         bool isRepeating() const;
 
         /**
          * setName(...)
-         * @param name The name of the Sound, can be any Text
+         * \param name The name of the Sound, can be any Text
         */
         void setName(const std::string &name);
 
--- inc/ui/ui_soundSettings.h
+++ inc/ui/ui_soundSettings.h
@@ -24,37 +24,37 @@ class UI_SoundSettings : public QWidget
 
         /**
          * getSource()
-         * @return Returns the source object to the .mp3 file
+         * \return Returns the source object to the .mp3 file
          */
         const SoundSource &getSource() const;
 
         /**
          * getVolume()
-         * @return Returns the volume of the sound
+         * \return Returns the volume of the sound
          */
         float getVolume() const;
 
         /**
          * getPlaybackSpeed()
-         * @return Returns the playback speed of the sound
+         * \return Returns the playback speed of the sound
          */
         float getPlaybackSpeed() const;
 
         /**
          * getLoops()
-         * @return Returns the amount of loops the sound will play
+         * \return Returns the amount of loops the sound will play
          */
         unsigned int getLoops() const;
 
         /**
          * getName()
-         * @return Returns the name of the sound
+         * \return Returns the name of the sound
          */
         const std::string &getName() const;
 
         /**
          * isRepeating()
-         * @return true  If the sound is in loop mode
+         * \return true  If the sound is in loop mode
          *         false If the sound is not in loop mode
          */
         bool isRepeating() const;
@@ -66,72 +66,72 @@ class UI_SoundSettings : public QWidget
     signals:
         /**
          * onPlaybackFinished()
-         * @brief Will be called if the playback is finished
+         * \brief Will be called if the playback is finished
          */
         void onPlaybackFinished();
 
         /**
          * onPlaybackPaused()
-         * @brief Will be called if the playback is paused
+         * \brief Will be called if the playback is paused
          */
         void onPlaybackPaused();
 
         /**
          * onPlaybackStopped()
-         * @brief Will be called if the playback is stopped
+         * \brief Will be called if the playback is stopped
          */
         void onPlaybackStopped();
 
     public slots:
         /**
          * play()
-         * @brief Will start the playback
+         * \brief Will start the playback
          */
         void play();
 
         /**
          * pause()
-         * @brief Will pause the current playback
+         * \brief Will pause the current playback
          */
         void pause();
 
         /**
          * stop()
-         * @brief Will stop the current playback
+         * \brief Will stop the current playback
          */
         void stop();
 
         /**
          * setSource(...)
-         * @param source The source object which contains the path to
+         * \param source The source object which contains the path to
          *               the .mp3 file
          */
         void setSource(const SoundSource &source);
 
         /**
          * setVolume(...)
-         * @param volume The volume of the playback.
+         * \param volume The volume of the playback.
          *               Range [0 - 1]
          */
         void setVolume(float volume);
 
         /**
          * setPlaybackSpeed(...)
-         * @param speed The speed of the playback.
+         * \param speed The speed of the playback.
          *              Default = 1, x<1 = faster
          */
         void setPlaybackSpeed(float speed);
 
         /**
          * setLoops(...)
-         * @param count The amount for how often the playback will be repeated.
+         * \param count The amount for how often the playback will be repeated.
          *              Sound::Loops::Infinite for infinite playback
          */
         void setLoops(int count);
 
         /**
          * toggleRepeat()
-         * @brief Will toggle the autorepeat mode
+         * \brief Will toggle the autorepeat mode
          */
         void toggleRepeat(bool toggleOn);
 
@@ -139,7 +139,7 @@ class UI_SoundSettings : public QWidget
 
         /**
          * setName(...)
-         * @param name The name of the Sound, can be any Text
+         * \param name The name of the Sound, can be any Text
         */
         void setName(const std::string &name);
 
