1028640fa8b8626b4adeeccbcf5e4cac518ea0f0
Alex Krieg <alexkrieg@gmx.ch>
2022-12-21 23:30:50 +0100

    -

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -31,9 +31,11 @@ const ProjectInfo projectInfo{"SoundBoard",
 
 int main(int argc, char *argv[]) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
     // Enable scaling for high resolution displays
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
 #endif
     QApplication a(argc, argv);
 
